ef990431e41fe4ea946ee557982e15cba391f747
Master-Megatron <102983156+Master-Megatron@users.noreply.github.com>
2022-04-22 06:20:43 +0700

    first commit

---
+++ src/services/inMemory/AlbumsService.js
@@ -0,0 +1,52 @@
+const { nanoid } = require('nanoid');
+
+class AlbumsService {
+  constructor() {
+    this._albums = [];
+  }
+
+  addAlbum({ name, year }) {
+    const id = nanoid(16);
+    const newAlbum = {
+      id, name, year,
+    };
+    this._albums.push(newAlbum);
+    const isSuccess = this._albums.filter((album) => album.id === id).length > 0;
+    if (!isSuccess) {
+      throw new Error('Album Gagal ditambahkan');
+    }
+    return id;
+  }
+
+  getAlbumById(id) {
+    const album = this._albums.filter((n) => n.id === id)[0];
+    if (!album) {
+      throw new Error('Album tidak ditemukan');
+    }
+    return album;
+  }
+
+  editAlbumById(id, { name, year }) {
+    const index = this._albums.findIndex((album) => album.id === id);
+
+    if (index === -1) {
+      throw new Error('Gagal memperbarui catatan. Id tidak ditemukan');
+    }
+
+    this._albums[index] = {
+      ...this._albums[index],
+      name,
+      year,
+    };
+  }
+
+  deleteAlbumById(id) {
+    const index = this._albums.findIndex((album) => album.id === id);
+    if (index === -1) {
+      throw new Error('Catatan gagal dihapus. Id tidak ditemukan');
+    }
+    this._albums.splice(index, 1);
+  }
+}
+
+module.exports = AlbumsService;
