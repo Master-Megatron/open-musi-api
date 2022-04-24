3825e1ddfaff708592441ae33c9f1bfe45fef241
Master-Megatron <102983156+Master-Megatron@users.noreply.github.com>
2022-04-24 18:25:55 +0700

    postgres database implemented for songs

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+// Album
 const albums = require('./api/albums');
 const AlbumsService = require('./services/postgres/AlbumsService');
 const AlbumValidator = require('./validator/albums');
 
+// song
+const songs = require('./api/songs');
+const SongsService = require('./services/postgres/SongsService');
+const SongValidator = require('./validator/songs');
+
 const init = async () => {
   const albumsService = new AlbumsService();
+  const songsService = new SongsService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -17,13 +24,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: albums,
-    options: {
-      service: albumsService,
-      validator: AlbumValidator,
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumValidator,
+      },
     },
-  });
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
